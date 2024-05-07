5f926b5cefb70c13cc2ca0bfd6c4aa3b9a88c21f
dgetahun473 <49160248+dgetahun473@users.noreply.github.com>
2024-05-07 18:11:35 +0300

    Update user-data.js

--- data/user-data.js
+++ data/user-data.js
@@ -1,13 +1,13 @@
 export const userData = {
-  githubUser: 'said7388',
-  devUsername: "said7388",
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  githubUser: 'dgetahun473',
+  devUsername: "dgetahun473",
+  github: 'https://github.com/dgetahun473',
+ // facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/dgetahun473/',
+  twitter: 'https://twitter.com/dgetahun473',
+ // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+ // leetcode: "https://leetcode.com/said3812/",
+  // resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
   skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
-  timezone: '+6'
+  timezone: '+3'
 };
